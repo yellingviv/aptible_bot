c22e22df09a9b555a47e3137e97f777fa0a2143c
yellingviv <viv@yellingviv.com>
2021-06-04 13:46:44 -0700

    hey ho let's go first commit

---
+++ aptible_bot.py
@@ -0,0 +1,21 @@
+# this is a friendly lil bot for room request approvals in aptible
+# it sends requests into slack and allows authorized users to approve from within slack
+# this is something of a beta test of the new API so let's be kind :)
+
+import requests
+import time
+from dotenv import load_dotenv
+
+
+
+# check for active requests in queue
+
+# retrieve ids of active requests in queue
+
+# get information on active requests
+
+# approve a request
+
+# for purposes of cli testing, get user info
+
+# for purposes of cli testing, serve option menu
