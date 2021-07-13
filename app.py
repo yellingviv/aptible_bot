6e922b1879b13d107698f7046b95cae6d9882dcd
yellingviv <viv@yellingviv.com>
2021-07-13 15:46:31 -0700

    gettin' that functionality heckin yeah

--- app.py
+++ app.py
@@ -18,25 +18,41 @@ app = App(
 logger = logging.getLogger(__name__)
 channel_id = os.getenv('SLACK_CHANNEL_ID')
 
-try:
-    result = app.client.chat_postMessage(
-        channel=channel_id,
-        blocks=slack_messages.new_request,
-        text="in case of emergency, break glass"
-    )
-    logger.info(result)
 
-except SlackApiError as e:
-    print(f"Error: {e}")
+queue_info = aptible_bot.pending_request_check()
+
+if queue_info != []:
+    for i in range(0, len(slack_messages.queue_blocks)):
+        try:
+            result = app.client.chat_postMessage(
+                channel=channel_id,
+                blocks=slack_messages.queue_blocks[i],
+                text="in case of emergency, break glass"
+            )
+            logger.info(result)
+        except SlackApiError as e:
+            print(f"Error: {e}")
 
 
-@app.action("button_click")
-def update_permissions(ack, body, payload):
+@app.action("approve")
+def handle_approval(ack, body, respond):
     ack()
-    print('did i succed')
-    print(payload)
-    say("I clicked it")
+    print('approve button go clicky')
+    respond(f"<@{['selected_user']}> made the approve button go clicky")
 
+
+@app.action("reject")
+def handle_rejection(ack, body, logger):
+    ack()
+    print('reject button go clicky')
+
+
+@app.action("perms")
+def handle_perm_ticks(ack, body, client):
+    ack()
+    get_email = client.users_info(user=body['user']['id'])
+    user_email = get_email['user']['profile']['email']
+    print(user_email)
 
 # start the app
 if __name__ == "__main__":
