21a0c78a09c96ac9c5e897fc828915e46dc8c0e1
yellingviv <viv@yellingviv.com>
2021-07-13 18:13:21 -0700

    yeah so i had an error in here that broke everything but now i don't it's cool

--- slack_messages.py
+++ slack_messages.py
@@ -6,71 +6,79 @@ import aptible_bot
 reqs = aptible_bot.show_queue_info()
 queue_blocks = []
 
-for i in range(0, len(reqs)):
-    if reqs[i]['status'] == 'requested':
-        req_block =[
-            {
-                "type": "header",
-                "text": {
-                    "type": "plain_text",
-                    "text": ":sparkles: Aptible Room Request :sparkles:"
-                }
-            },
-            {
-                "type": "section",
-                "text": {
-                    "type": "mrkdwn",
-                    "text": "<" + reqs[i]['url'] + "|New request> from " + reqs[i]['from']
-                }
-            },
-            {
-                "type": "section",
-                "text": {
-                    "type": "mrkdwn",
-                    "text": "Requested received at: " + reqs[i]['time']
-                }
-            },
-            {
-                "type": "section",
-                "text": {
-                    "type": "mrkdwn",
-                    "text": "Message: " + reqs[i]['message']
-                }
-            },
-            {
-                "type": "input",
-                "element": {
-                    "type": "checkboxes",
-                     "options": aptible_bot.get_perms(),
-                    "action_id": "perms"
-                },
-                "label": {
-                    type: "plain_text",
-                    "text": "All requests are granted standard permissions. Use the checkboxes to add additional permissions if needed."
-                }
-            },
-            {
-                "type": "actions",
-                "elements": [
-                    {
-                        "type": "button",
-                        "text": {
-                            "type": "plain_text",
-                            "text": "Approve Request"
-                        },
-                        "value": "yes",
-                        "action_id": "approve"
-                    },
-                    {
-                        "type": "button",
-                        "text": {
-                            "type": "plain_text",
-                            "text": "Reject Request"
-                        },
-                        "value": "no",
-                        "action_id": "reject"
+def create_queue():
+
+    for i in range(0, len(reqs)):
+        print('looking at req ', i, ' of ', len(reqs))
+        if reqs[i]['status'] == 'requested':
+            req_block =[
+                {
+                    "type": "header",
+                    "text": {
+                        "type": "plain_text",
+                        "text": ":sparkles: Aptible Room Request :sparkles:"
                     }
-                ]
-            }
-        ]
-        queue_blocks.append(req_block)
+                },
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": "<" + reqs[i]['url'] + "|New request> from " + reqs[i]['from']
+                    }
+                },
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": "Requested received at: " + reqs[i]['time']
+                    }
+                },
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": "Message: " + reqs[i]['message']
+                    }
+                },
+                {
+                    "type": "input",
+                    "element": {
+                        "type": "checkboxes",
+                         "options": aptible_bot.get_perms(),
+                        "action_id": "perms"
+                    },
+                    "label": {
+                        "type": "plain_text",
+                        "text": "All requests are granted standard permissions. Use the checkboxes to add additional permissions if needed."
+                    }
+                },
+                {
+                    "type": "actions",
+                    "elements": [
+                        {
+                            "type": "button",
+                            "text": {
+                                "type": "plain_text",
+                                "text": "Approve Request"
+                            },
+                            "value": "yes",
+                            "action_id": "approve"
+                        },
+                        {
+                            "type": "button",
+                            "text": {
+                                "type": "plain_text",
+                                "text": "Reject Request"
+                            },
+                            "value": "no",
+                            "action_id": "reject"
+                        }
+                    ]
+                }
+            ]
+            queue_blocks.append(req_block)
+        else:
+            print("Queue not included: ")
+            print(reqs[i]['name'], reqs[i]['status'])
+
+    return queue_blocks
