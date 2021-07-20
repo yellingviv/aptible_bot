3b1c4a9e69572247cfc4466c48d78f44f6fd1ac1
yellingviv <viv@yellingviv.com>
2021-07-20 16:51:56 -0700

    approvals are creeping ever closer

--- app.py
+++ app.py
@@ -23,7 +23,8 @@ channel_id = os.getenv('SLACK_CHANNEL_ID')
 queue_info = aptible_bot.pending_request_check()
 
 if queue_info != []:
-    queue = slack_messages.create_queue()
+    reqs = aptible_bot.show_queue_info()
+    queue = slack_messages.create_queue(reqs)
     for i in range(0, len(queue)):
         try:
             result = app.client.chat_postMessage(
@@ -32,6 +33,7 @@ if queue_info != []:
                 text="you should only see this if something went wrong, which means something went wrong. Please contact @vivienne!"
             )
             logger.info(result)
+            # aptible_bot.status_update(reqs[i]['id'], 'filler')
         except SlackApiError as e:
             print(f"Error: {e}")
 
@@ -44,13 +46,14 @@ def handle_approval(ack, body, client, say):
     addtl_perms = aptible_bot.get_selections(payload, selections)
     if addtl_perms == 'yikes':
         say("Something went wrong with the permissions. Please contact @vivienne.")
-    get_email = client.users_info(user=body['user']['id'])
-    user_email = get_email['user']['profile']['email']
+    # get_email = client.users_info(user=body['user']['id'])
+    # user_email = get_email['user']['profile']['email']
+    user_email = 'vpustell@pagerduty.com'
     request_id = body['actions'][0]['value']
     approve_it = aptible_bot.approve_requests(request_id, user_email, addtl_perms)
     if approve_it == 'yay':
         say("Approval success!")
-        update_request_screen(body['container'])
+        update_request_screen(body['container'], client)
     else:
         print(approve_it)
         try:
@@ -72,7 +75,7 @@ def handle_rejection(ack, body, client):
 
 
 @app.action("perms")
-def handle_perm_ticks(ack, body, client):
+def handle_perm_ticks(ack):
     ack()
     print('they messin with them boxes again')
 
@@ -83,10 +86,12 @@ def update_request_screen(msg_info, client):
         result = app.client.chat_update(
             channel=channel_id,
             ts=ts,
-            blocks=[{"type": "section", "text": {"type": "plain_text", "text": "hahahaha maniacal laughter"}}]
+            blocks=[{"type": "section", "text": {"type": "plain_text", "text": "hahahaha maniacal laughter"}}],
+            text="You have successfully approved this request."
         )
     except SlackApiError as e:
         print(f"Error: {e}")
+
 
 # start the app
 if __name__ == "__main__":
