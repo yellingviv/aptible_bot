3f0bc06c88735d761e5c35fae6d9ac89c38d2a11
yellingviv <viv@yellingviv.com>
2021-07-22 16:58:49 -0700

    more messages for more interactivity getting chatty getting chatty aww yiss

--- slack_messages.py
+++ slack_messages.py
@@ -5,102 +5,108 @@ import aptible_bot
 from datetime import datetime
 
 def create_queue(reqs):
+    # create a whole bunch of properly formatted blocks for new requests
+    # this is here because gosh it takes up a lot of space
 
     queue_blocks = []
 
     for i in range(0, len(reqs)):
-        if reqs[i]['status'] == 'requested':
-            req_block =[
-                {
-                    "type": "header",
-                    "text": {
-                        "type": "plain_text",
-                        "text": ":sparkles: Aptible Room Request :sparkles:"
-                    }
-                },
-                {
-                    "type": "section",
-                    "text": {
-                        "type": "mrkdwn",
-                        "text": "<" + reqs[i]['url'] + "|New request> from " + reqs[i]['from']
-                    }
-                },
-                {
-                    "type": "section",
-                    "text": {
-                        "type": "mrkdwn",
-                        "text": "Requested received at: " + reqs[i]['time']
-                    }
-                },
-                {
-                    "type": "section",
-                    "text": {
-                        "type": "mrkdwn",
-                        "text": "Message: " + reqs[i]['message']
-                    }
-                },
-                {
-                    "type": "input",
-                    "element": {
-                        "type": "checkboxes",
-                         "options": aptible_bot.get_perms(),
-                        "action_id": "perms"
-                    },
-                    "label": {
-                        "type": "plain_text",
-                        "text": "All requests are granted standard permissions. Use the checkboxes to add additional permissions if needed."
-                    }
-                },
-                {
-                    "type": "actions",
-                    "elements": [
-                        {
-                            "type": "button",
-                            "text": {
-                                "type": "plain_text",
-                                "text": "Approve Request"
-                            },
-                            "value": reqs[i]['id'],
-                            "action_id": "approve"
-                        },
-                        {
-                            "type": "button",
-                            "text": {
-                                "type": "plain_text",
-                                "text": "Reject Request"
-                            },
-                            "value": reqs[i]['id'],
-                            "action_id": "reject"
-                        }
-                    ]
+        req_block =[
+            {
+                "type": "header",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": ":sparkles: <" + requs[i]['url'] + "|Aptible Room Request> :sparkles:"
                 }
-            ]
-            queue_blocks.append(req_block)
-        else:
-            print("Queue not included: ")
-            print(reqs[i]['name'], reqs[i]['status'])
+            },
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": "From: " + reqs[i]['from']
+                }
+            },
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": "Received at: " + reqs[i]['time']
+                }
+            },
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": "Message: " + reqs[i]['message']
+                }
+            },
+            {
+                "type": "input",
+                "element": {
+                    "type": "checkboxes",
+                     "options": aptible_bot.get_perms(),
+                    "action_id": "perms"
+                },
+                "label": {
+                    "type": "plain_text",
+                    "text": "All requests are granted standard permissions. Use the checkboxes to add additional permissions if needed."
+                }
+            },
+            {
+                "type": "actions",
+                "elements": [
+                    {
+                        "type": "button",
+                        "text": {
+                            "type": "plain_text",
+                            "text": "Approve Request"
+                        },
+                        "value": reqs[i]['id'],
+                        "action_id": "approve"
+                    },
+                    {
+                        "type": "button",
+                        "text": {
+                            "type": "plain_text",
+                            "text": "Reject Request"
+                        },
+                        "value": reqs[i]['id'],
+                        "action_id": "reject"
+                    }
+                ]
+            }
+        ]
+        queue_blocks.append(req_block)
 
     return queue_blocks
 
 
-def update_request_approved(from, user):
+def update_request(requester, user, status, note="N/A"):
+    # create a block that updates the original request message to show status
 
     approved_time = datetime.now()
     stamp = approved_time.strftime("%B %d, %Y, %I:%M %p")
-    approved_blocks = [
+    status_blocks = [
         {
             "type": "header",
             "text": {
                 "type": "plain_text",
-                "text": ":sparkles: Aptible Room Request :sparkles:"
+                "text": ":sparkles:" + status.upper() + " Aptible Room Request :sparkles:"
             }
         },
         {
             "type": "section",
             "text": {
                 "type": "mrkdwn",
-                "text": "Request from " ugh + " approved by " + user + " at " + stamp
+                "text": "Request from " requester + " " + status + "ed by " + user + " at " + stamp + "."
+            }
+        },
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": "Request reviewed with comment: " + note
             }
         }]
 
-    return approved_blocks
+    return status_blocks
